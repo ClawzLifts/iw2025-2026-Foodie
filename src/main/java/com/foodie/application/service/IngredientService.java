d5c8f53609165912da8cfcc9933efc69abc34726
JaimeBM <jaime.bellidomedina@alum.uca.es>
2025-12-27 13:55:50 +0100

    Add IngredientRepository and IngredientService for ingredient management

---
+++ src/main/java/com/foodie/application/service/IngredientService.java
@@ -0,0 +1,59 @@
+package com.foodie.application.service;
+
+import com.foodie.application.domain.Allergen;
+import com.foodie.application.domain.Ingredient;
+import com.foodie.application.repository.IngredientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class IngredientService {
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientService(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    public List<Ingredient> getAllIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    @Transactional
+    public Ingredient createIngredient(String ingredientName) {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setName(ingredientName);
+
+        return ingredientRepository.save(ingredient);
+    }
+
+    @Transactional
+    public Set<Ingredient> findOrCreateByNames(Set<String> names) {
+        if (names == null || names.isEmpty()) {
+            return Set.of();
+        }
+        return names.stream()
+                .map(name -> ingredientRepository.findByName(name)
+                        .orElseGet(() -> ingredientRepository.save(Ingredient.builder().name(name).build())))
+                .collect(Collectors.toSet());
+    }
+
+    @Transactional
+    public void updateIngredientName(Integer ingredientId, String newName) {
+        var ingredient = ingredientRepository.findById(ingredientId)
+                .orElseThrow(() -> new EntityNotFoundException("Ingredient not found with id: " + ingredientId));
+        ingredient.setName(newName);
+    }
+
+    @Transactional
+    public  void deleteIngredient(Integer ingredientId) {
+        if (!ingredientRepository.existsById(ingredientId)) {
+            throw new EntityNotFoundException("Allergen not found with id: " + ingredientId);
+        }
+        ingredientRepository.deleteById(ingredientId);
+    }
+}
