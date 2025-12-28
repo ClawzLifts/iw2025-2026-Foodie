698d46a6d79f491a695f92f1adbcbef2031b2ff3
JaimeBM <jaime.bellidomedina@alum.uca.es>
2025-12-28 16:56:21 +0100

    Add method to find ingredients by a set of names in IngredientRepository

--- src/main/java/com/foodie/application/repository/IngredientRepository.java
+++ src/main/java/com/foodie/application/repository/IngredientRepository.java
@@ -4,7 +4,9 @@ import com.foodie.application.domain.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface IngredientRepository extends JpaRepository<Ingredient,Integer> {
     Optional<Ingredient> findByName(String name);
+    Set<Ingredient> findByNameIn(Set<String> names);
 }
