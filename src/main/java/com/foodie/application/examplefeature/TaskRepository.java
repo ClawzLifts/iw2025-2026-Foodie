f7970aabb39772df4d0b765944e82e64395774f3
Joseelv_ <jose.venegasan@gmail.com>
2025-12-30 18:51:31 +0100

    Eliminar la entidad Task y su repositorio y servicio, así como la configuración de H2 para pruebas, se ha modificado CI para que cumpla con la rúbrica y BD postgres

--- src/main/java/com/foodie/application/examplefeature/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foodie.application.examplefeature;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
